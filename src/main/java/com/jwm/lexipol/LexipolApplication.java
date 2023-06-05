027c9d2914264246eb084eca31b6470e289d7a23
JWM <wallymill@yahoo.com>
2023-06-04 20:38:36 -0500

    Executor is working - just need to upload

--- src/main/java/com/jwm/lexipol/LexipolApplication.java
+++ src/main/java/com/jwm/lexipol/LexipolApplication.java
@@ -1,20 +1,43 @@
 package com.jwm.lexipol;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
+@EnableAsync
 public class LexipolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LexipolApplication.class, args);
+  @Autowired
+  private BeanFactory beanFactory;
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(LexipolApplication.class, args);
+  }
+
+  @Bean(name = "multipartResolver")
+  public CommonsMultipartResolver commonsMultipartResolver() {
+    return new CommonsMultipartResolver();
+  }
+
+
+  @Bean
+  public Executor executor() {
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(1);
+		threadPoolTaskExecutor.setMaxPoolSize(1);
+		threadPoolTaskExecutor.initialize();
+		return new LazyTraceExecutor(beanFactory, threadPoolTaskExecutor);
 	}
 
-	@Bean(name = "multipartResolver")
-	public CommonsMultipartResolver commonsMultipartResolver(){
-		return new CommonsMultipartResolver();
-	}
-
-}
+  }
--- src/main/java/com/jwm/lexipol/service/FileUploadService.java
+++ src/main/java/com/jwm/lexipol/service/FileUploadService.java
@@ -1,8 +1,11 @@
 package com.jwm.lexipol.service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,8 +14,21 @@ public class FileUploadService {
 
   Logger logger = LoggerFactory.getLogger(FileUploadService.class);
 
+  private final Executor executor;
+
+  public FileUploadService(final Executor executor) {
+    this.executor = executor;
+  }
+
   public void uploadFiles(String userId, List<MultipartFile> files) {
     logger.info("FileUploadService.uploadFiles:  userId:  {} files:  {}", userId, files);
+    files.stream().map(file -> CompletableFuture.supplyAsync(() -> uploadFile(userId, file), executor))
+        .map(CompletableFuture::join).forEach(s -> logger.info(s.toString()));
+  }
+
+  public HttpStatus uploadFile(String userId, MultipartFile file) {
+    logger.info("here I am:  userId:  {}  file:  {}", userId, file);
+    return HttpStatus.OK;
   }
 
 }
